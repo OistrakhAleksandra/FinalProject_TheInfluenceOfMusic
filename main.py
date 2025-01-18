a978338bb967c1c9dfb97aca367a7f2d63b53515
Your Name <your_email@example.com>
2025-01-18 14:23:15 +0200

    Added libraries and changes in folders names

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-
-from tools.data_filter import filter_to_new_excel  # Import the function
+from data_filter import filter_to_new_excel  # Import the function
 
 # Define folder path and selected columns
 folder_path = r"C:\Users\Home\Desktop\Studies\Phyton\projects 2024-2025\Project_2\data\trial_data"
-selected_columns = ['session', 'music_type', 'valence_rating', 'RT']  # Specify the columns you need
+selected_columns = ["session", "music_type", "valence_rating", "RT"]  # Specify the columns you need
 final_folder_path = r"C:\Users\Home\Desktop\Studies\Phyton\projects 2024-2025\Project_2\data"
 
 # Call the function from functions.py
