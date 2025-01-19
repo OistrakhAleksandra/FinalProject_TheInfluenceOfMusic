b84cbddd02d6a19d784b9c0d2d06ba8b52d46df6
Your Name <your_email@example.com>
2025-01-19 09:06:10 +0200

    ppg_combined bug fixed

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-# Import the functions
-from .functions.clearing_data.ppg_combined import combine_ppg_averages
-from .functions.clearing_data.trial_combined import filter_to_new_excel
+"""This script is the main code with all the functions neede to filter the data.
 
-# Define folder path and selected columns
+It uses the combine_ppg_averages function to calculate PPG averages and the filter_to_new_excel function
+to filter the trial data based on selected columns.
+"""
+
+# Import the functions
+from functions.clearing_data.ppg_combined import combine_ppg_averages
+from functions.clearing_data.trial_combined import filter_to_new_excel
+
+# Path to the folder with trial data
 folder_path = r"C:\Users\Home\Desktop\Studies\Phyton\projects 2024-2025\Project_2\data\trial_data"
-input_folder = r"C:\Users\Home\Desktop\Studies\Phyton\projects 2024-2025\Project_2\data\PPG_data"  # Path to the folder with PPG files
-selected_columns = ["session", "music_type", "valence_rating", "RT"]  # Specify the columns you need
+# Path to the folder with PPG file
+input_folder = r"C:\Users\Home\Desktop\Studies\Phyton\projects 2024-2025\Project_2\data\PPG_data"
+# Columns for combined_trial
+selected_columns = ["session", "music_type", "valence_rating", "RT"]
+# Path to the final data folder
 final_data_path = r"C:\Users\Home\Desktop\Studies\Phyton\projects 2024-2025\Project_2\data"
-combined_data_path = r"C:\Users\Home\Desktop\Studies\Phyton\projects 2024-2025\Project_2\data\combined_data.xlsx"
+# Columns for PPG_combined data
 needed_columns = ["valence_rating", "RT"]
 
 # Call the function from functions.py
