82fc504f4ea8b657120e68d978c0775a461a5de9
Your Name <your_email@example.com>
2025-01-29 21:51:14 +0200

    git ignore and main modified

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ from functions.clearing_data.adding_ppg_to_trial_comb import match_ppg_data
 from functions.clearing_data.calc_r_peaks_from_ppg import process_ppg_folder
 from functions.clearing_data.is_to_excel import calculate_is
 from functions.clearing_data.trial_combined import filter_to_new_excel
+from functions.data_analysis.Average_valence_scrore_to_IS import analyze_valence_vs_IS
+from functions.data_analysis.HB_by_music_type import plot_hr_by_music_type
+from functions.data_analysis.is_to_music_type import analyze_music_type_vs_IS
+from functions.data_analysis.music_type_to_rt import analyze_rt_by_music_type
+from functions.data_analysis.valence_rating_by_music_type import plot_valence_by_music_type
 
 # Path to the final data folder
 final_data_path = r"C:\Users\Home\Desktop\Studies\Phyton\projects 2024-2025\Final_project\data"
@@ -34,8 +39,8 @@ process_ppg_folder(PPG_data_path, final_data_path)
 calculate_is(HBD_data_path, combined_data_file)
 
 # Call the analyzing functions
-# plot_valence_by_music_type(combined_data_file)
-# plot_hr_by_music_type(R_peak_data_path, combined_data_file)
-# analyze_music_type_vs_IS(combined_data_file)
-# analyze_rt_by_music_type(combined_data_file)
-# analyze_valence_vs_IS(combined_data_file)
+plot_valence_by_music_type(combined_data_file)
+plot_hr_by_music_type(R_peak_data_path, combined_data_file)
+analyze_music_type_vs_IS(combined_data_file)
+analyze_rt_by_music_type(combined_data_file)
+analyze_valence_vs_IS(combined_data_file)
